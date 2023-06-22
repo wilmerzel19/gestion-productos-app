22fec567c4e78942b48117510d6705ecd6918577
wilmer <est.wzelayam@uml.edu.ni>
2023-06-22 13:28:42 -0600

    first commit

--- src/components/layoust/Logo.tsx
+++ src/components/layoust/Logo.tsx
@@ -8,7 +8,7 @@ export default function Logo({}: Props) {
     
     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
     <div className="w-10 rounded-full">
-      <img src="img/logo1.png" />
+      <img src="src/assets/img/logo1.png" />
     </div>
   </label>
   
