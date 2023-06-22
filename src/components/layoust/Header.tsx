f1f0cd3a9fcd941aa197a79f6f1aa416fc31b01b
wilmer <est.wzelayam@uml.edu.ni>
2023-06-22 13:25:42 -0600

    first commit

--- src/components/layoust/Header.tsx
+++ src/components/layoust/Header.tsx
@@ -6,25 +6,32 @@ type Props = {}
 export default function Header({}: Props) {
   return (
 
-<div className="navbar w-auto bg-cyan-600">
+    
+
+<div className="navbar bg-cyan-600">
   <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl text-white"><Logo /> Gestion de Productos</a>
-  </div>  
+    <a className="btn btn-ghost normal-case text-xl"><Logo/>Productos</a>
+  </div>
   <div className="flex-none">
-    <ul className="menu menu-horizontal  px-1 text-white">
-      <li><a>Home</a></li>
-      <li>
-        <details>
-          <summary>
-            Productos
-          </summary>
-          <ul className="p-2 bg-slate-600 text-blue-900  ">
-            <li><a>account</a></li>
-            <li><a>Link</a></li>
-          </ul>
-        </details>
-      </li>
-    </ul>
+    <div className="dropdown dropdown-end">
+     
+    </div>
+    <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="btn btn-ghost">
+        <div className="w-10 rounded-full">
+        <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+        </div>
+      </label>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
   </div>
 </div>
 
--- src/components/layoust/Logo.tsx
+++ src/components/layoust/Logo.tsx
@@ -6,7 +6,11 @@ type Props = {}
 export default function Logo({}: Props) {
   return (
     
-  <img src="vite.svg"/>
+    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+    <div className="w-10 rounded-full">
+      <img src="img/logo1.png" />
+    </div>
+  </label>
   
 
   )
